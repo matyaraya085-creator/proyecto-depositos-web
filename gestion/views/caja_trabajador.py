a80f43c3558d7e64b396224cf19d2739329b0fb2
matyaraya085-creator <matyaraya085@gmail.com>
2025-12-06 01:22:40 -0300

    Fix: Ahora sí existe la funcion form_rendicion en views

--- gestion/views/caja_trabajador.py
+++ gestion/views/caja_trabajador.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-# --- VISTA DEL MENÚ ---
+# --- MÓDULO DE CAJA Y FLUJO POR TRABAJADOR ---
+
 @login_required
 def menu_trabajadores(request):
-    # CORRECCIÓN: Ahora apuntamos a la carpeta 'caja_trabajador'
+    """
+    Vista principal del menú.
+    """
     return render(request, 'gestion/caja_trabajador/menu_trabajador.html')
+
+@login_required
+def form_rendicion(request):
+    """
+    Vista para el formulario de rendición (Diseño visual).
+    """
+    return render(request, 'gestion/caja_trabajador/form_rendicion.html')
